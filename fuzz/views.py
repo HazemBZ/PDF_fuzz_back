fc54c123d2e9d641ff5137c9440ead0055e910a5
HazemBZ <hazembenbz@gmail.com>
2024-08-12 19:32:11 +0100

    update: Migrate search to use es interface

--- fuzz/views.py
+++ fuzz/views.py
@@ -2,15 +2,15 @@ import json
 import pathlib
 
 from django.http import FileResponse, JsonResponse
-from PDF_Fuzz.settings import ASSETS_DIR, IMAGES_DIR
+from PDF_Fuzz.settings import IMAGES_DIR
 
 from fuzz.utils.file_utils import get_files_from_folder, get_pdf_files_paths_list
-from fuzz.utils.pdf_utils import get_keyword_matches_page_numbers
-from fuzz.utils.setup_utils import check_images_folder, check_processed_files
+from fuzz.search import Search
+from itertools import groupby
 
 # TODO: Delegate this to an endpoint that unloads file processing to a task queue
-check_images_folder(IMAGES_DIR)
-check_processed_files(ASSETS_DIR)
+# check_images_folder(IMAGES_DIR)
+# check_processed_files(ASSETS_DIR)
 
 
 # =========== API ================
@@ -45,32 +45,26 @@ def get_images_by_keyword(request):
         return JsonResponse({"message": "please POST request in `JSON` format"})
 
     req = json.loads(body)
+    keyword = req["keyword"]
+    
+    def build_image_name(path, count):
+        file_name = path.split('/')[-1].replace('.pdf', '.jpg')
+        stripped_name = file_name.replace('.jpg', '')
+        return f"{stripped_name}/{count}_{file_name}"
+        
+    res = Search.get_matching_keyword(keyword)['hits']['hits']
+    
+    formatted = []
+    
+    for key, items in groupby(res, lambda x: x['_source']['file_path']):
+        match_group = {}
+        
+        match_group['file'] = key
+        match_group['matchedImages'] = [build_image_name(key, item['_source']['page_id']) for item in items ]
+        match_group['keyword'] = keyword
+        formatted.append(match_group)
 
-    matched_pages_images = []
-    for file in req["files"]:
-        keyword = req["keyword"]
-        image_matches = get_keyword_matches_page_numbers(pathlib.Path(file), keyword)
-
-        matched_pages_images.append(
-            {
-                "file": file,
-                "keyword": keyword,
-                "matchedImages": list(
-                    map(
-                        lambda x: f"{request.build_absolute_uri().split('image')[0]}image/path/{str(x).replace('images/','')}",
-                        filter(
-                            lambda path: int(path.name.split("_")[0]) in image_matches,
-                            get_files_from_folder(
-                                pathlib.Path(IMAGES_DIR, pathlib.Path(file).stem),
-                                "jpg",
-                            ),
-                        ),
-                    )
-                ),
-            }
-        )
-
-    return JsonResponse(matched_pages_images, safe=False)
+    return JsonResponse(formatted, safe=False)
 
 
 def get_image_from_path(request, image_path):
