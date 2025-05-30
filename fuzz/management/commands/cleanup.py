9abffb2989335192554ce8c81cc804491e14111f
hazembz <hazembenbz@gmail.com>
2025-05-30 19:52:55 +0100

    update: Add search index cleanup on cleanup command

--- fuzz/management/commands/cleanup.py
+++ fuzz/management/commands/cleanup.py
@@ -3,10 +3,12 @@ from django.core.management.base import BaseCommand
 from fuzz.utils.file_utils import FileManager
 from PDF_Fuzz.settings import IMAGES_DIR, UPLOADS_DIR
 from chunkedUpload.models import ChunkedUpload
+from fuzz.search import Search
 
 logger = logging.getLogger(__name__)
 
 
+# TODO: Cleanup es document index
 class Command(BaseCommand):
     help = "Cleanup uploads related data"
 
@@ -18,6 +20,7 @@ class Command(BaseCommand):
             FileManager.delete_path_recursively(UPLOADS_DIR)
             FileManager.delete_path_recursively(IMAGES_DIR)
             ChunkedUpload.objects.all().delete()
+            Search.create_index()
 
         except Exception:
             logger.exception("Failed to cleanup")
