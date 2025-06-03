15e9054328c728c1e860000513f9c2a35797da07
hazembz <hazembenbz@gmail.com>
2025-06-03 23:11:48 +0100

    chore: Setsup celery tasks queue infra

--- PDF_Fuzz/__init__.py
+++ PDF_Fuzz/__init__.py
@@ -1,4 +1,10 @@
 from fuzz.search import Search
+from PDF_Fuzz.celery import app as celery_app
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+
 
 if Search.es is None:
     Search.connect()
+
+__all__ = ("celery_app",)
---
+++ PDF_Fuzz/celery.py
@@ -0,0 +1,48 @@
+import logging
+import os
+
+from celery import Celery
+from celery.signals import setup_logging
+
+# from fuzz.search import Search
+from fuzz.tasks import test_connection
+
+logger = logging.getLogger(__name__)
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PDF_Fuzz.settings")
+
+app = Celery(
+    "PDF_Fuzz", backend="redis://redis:6379/0", broker="amqp://guest@rabbitmq//"
+)
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Search.connect()
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f"Request: {self.request!r}")
+
+
+@setup_logging.connect
+def config_loggers(*args, **kwags):
+    from logging.config import dictConfig
+
+    from django.conf import settings
+
+    dictConfig(settings.CELERY_LOGGING)
+
+
+if test_connection():
+    logger.info("✅ Successfully connected to Celery worker!")
+else:
+    logger.warn("❌ Connection to Celery worker failed")
