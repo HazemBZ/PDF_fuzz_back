a73bbd5aec867e74ec487ff3d8ee3238089e2f61
hazembz <hazembenbz@gmail.com>
2025-07-31 15:13:43 +0100

    refactor(format code)

--- fuzz/views.py
+++ fuzz/views.py
@@ -1,26 +1,20 @@
 import json
 import pathlib
+from itertools import groupby
 
 from django.http import FileResponse, JsonResponse
 from PDF_Fuzz.settings import IMAGES_DIR
 
-from fuzz.utils.file_utils import FileManager
 from fuzz.search import Search
-from itertools import groupby
-
-# TODO: Delegate this to an endpoint that unloads file processing to a task queue
-# check_images_folder(IMAGES_DIR)
-# check_processed_files(ASSETS_DIR)
+from fuzz.utils.file_utils import FileManager
 
 
-# =========== API ================
-
-# TODO: Refactor
-def get_all_file_names(
-    request,
-):
+def get_all_file_names(request):
     pdf_file_names = list(
-        map(lambda x: {"name": x.name, "path": str(x)}, FileManager.get_uploaded_files("*.pdf"))
+        map(
+            lambda x: {"name": x.name, "path": str(x)},
+            FileManager.get_uploaded_files("*.pdf"),
+        )
     )
     return JsonResponse(
         pdf_file_names,
@@ -28,7 +22,6 @@ def get_all_file_names(
     )
 
 
-# TODO: Refactor
 def get_all_images_by_file_name(request, fileName):
     image_obj = {
         "file": fileName,
@@ -42,7 +35,6 @@ def get_all_images_by_file_name(request, fileName):
     return JsonResponse(image_obj)
 
 
-# TODO: Refactor
 def get_images_by_keyword(request):
     body = request.body
     if not body:  # check if json later
@@ -50,22 +42,24 @@ def get_images_by_keyword(request):
 
     req = json.loads(body)
     keyword = req["keyword"]
-    
+
     def build_image_name(path, count):
-        file_name = path.split('/')[-1].replace('.pdf', '.jpg')
-        stripped_name = file_name.replace('.jpg', '')
+        file_name = path.split("/")[-1].replace(".pdf", ".jpg")
+        stripped_name = file_name.replace(".jpg", "")
         return f"{stripped_name}/{count}_{file_name}"
-        
-    res = Search.get_matching_keyword(keyword)['hits']['hits']
-    
+
+    res = Search.get_matching_keyword(keyword)["hits"]["hits"]
+
     formatted = []
-    
-    for key, items in groupby(res, lambda x: x['_source']['file_path']):
+
+    for key, items in groupby(res, lambda x: x["_source"]["file_path"]):
         match_group = {}
-        
-        match_group['file'] = key
-        match_group['matchedImages'] = [build_image_name(key, item['_source']['page_id']) for item in items ]
-        match_group['keyword'] = keyword
+
+        match_group["file"] = key
+        match_group["matchedImages"] = [
+            build_image_name(key, item["_source"]["page_id"]) for item in items
+        ]
+        match_group["keyword"] = keyword
         formatted.append(match_group)
 
     return JsonResponse(formatted, safe=False)
