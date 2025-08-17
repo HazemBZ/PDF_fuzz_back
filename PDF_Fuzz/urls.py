daabd230d939379a82f305fcae77406378766651
hazembz <hazembenbz@gmail.com>
2025-08-17 20:29:57 +0100

    update: Change chunkedUpload root uri

--- PDF_Fuzz/urls.py
+++ PDF_Fuzz/urls.py
@@ -22,6 +22,6 @@ from PDF_Fuzz import views as helper_views
 urlpatterns = [
     path("admin/", admin.site.urls), 
     path("api/fuzz/", include("fuzz.urls")),
-    path("chunkedUpload/", include("chunkedUpload.urls")),
+    path("api/chunkedUpload/", include("chunkedUpload.urls")),
     path("health/", helper_views.health_check, name='health_check')
     ]
